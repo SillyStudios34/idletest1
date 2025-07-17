4ae90f6893146ef38a2927dc6b4438af1e158479
HenryHere34 <userplx75@gmail.com>
2025-07-17 10:44:27 +0200

    part fixation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # idletest1
 boredom, bitch
+
+still need to fix the expo upgrade, then going to progress with other ones
--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 // section of values n stuff
 let wCredit = parseInt(localStorage.getItem("wCredit")) || 0;
 let wPerS = parseInt(localStorage.getItem("wPerS")) || 1;
-let upgPr = 130;
+let upgPr = parseInt(localStorage.getItem("upg1")) || 130;
 let upgPr3 = 600;
 let expoIn = false;
 let gamerun = false;
@@ -9,8 +9,8 @@ let idInter;
 let expo;
 //section 2
 let wAsideCredit = parseInt(localStorage.getItem("wAsideCredit")) || 0;
-let wAsidePerS = 0.1;
-let wAsideVar = false;
+let wAsidePerS = 0;
+let wAsideVar = parseInt(localStorage.getItem("asideVariable")) || false;
 let secondVar;
 // start this shi
 // also upgrades part 1
@@ -22,13 +22,8 @@ function gameSt() {
             saveDat();
             updateUI();
         }, 40);
-    console.log("game's running")
-    }
-    if (!expoIn) {
-        expo = setInterval (() => {
-            wPerS *= 1.02;
-            saveDat()
-        }, 4000);
+    console.log("game's running");
+    console.log(`upgrade 1 price is ${upgPr}`)
     }
 }
 function upg1n1() {
@@ -45,42 +40,65 @@ function upg1e1() {
     if (wCredit >= upgPr3) {
         wCredit -= upgPr3;
         expoIn = true;
+        console.log("expo bought");
+    }
+    if (expoIn = true) {
+        expo = setInterval (() => {
+            wPerS *= 1.02;
+            saveDat()
+            console.log("expo is true")
+        }, 4000);
+    }
+    else {
+        expoIn = null;
     }
 }
 // section 2 upgrades
-let asideUnlock = 1700;
+let asideUnlock = 17000;
 function convertWCr() {
     if (wCredit >= asideUnlock) {
         wCredit -= asideUnlock;
+        wAsidePerS = 0.5;
         wAsideVar = true;
+        console.log("second income unlocked");
+        saveDat();
     }
-    if (!wAsideVar) {
-        secondVar = setInterval (() => {
-            wAsideCredit += wAsidePerS;
-            saveDat();
-        }, 350);
+    else {
+        console.log("not enough");
+        wAsideVar = null;
     }
 }
+if (wAsideVar = true) {
+    secondVar = setInterval (() => {
+        wAsideCredit += wAsidePerS;
+        saveDat();
+    }, 350);
+}
+if (wAsideVar = true);
+console.log("already bought");
+
 function getKAside() {
-    if (wCredit > 500000) {
+    if (wCredit >= 50000) {
         wCredit = 0;
         wPerS = 1;
-        wAsideCredit += 10000
-        expoIn = false;
+        wAsideCredit += 10000;
+        expoIn = null;
+        clearInterval(expo);
     }
 }
 //format function (test only)
 function formattAll() {
-    localStorage.clear()
+    localStorage.clear();
     wCredit = 0;
     wPerS = 1;
     wAsideCredit = 0;
+    upgPr = 130;
     gamerun = null;
     expoIn = null;
-    wAsideVar = false;
-    clearInterval(gamerun);
-    clearInterval(expo);
-    clearInterval(secondVar);
+    wAsideVar = null;
+    clearInterval(gamerun)
+    clearInterval(expo)
+    clearInterval(secondVar)
     console.log("all gone!")
     updateUI();
 }
@@ -94,5 +112,7 @@ function saveDat() {
     localStorage.setItem("gamerun", gamerun);
     localStorage.setItem("wCredit", wCredit);
     localStorage.setItem("wPerS", wPerS);
+    localStorage.setItem("upg1", upgPr);
     localStorage.setItem("wAsideCredit", wAsideCredit);
+    localStorage.setItem("asideVariable", wAsideVar);
 }
